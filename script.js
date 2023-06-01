15de2a54c1bed1e331276aee1e1f2e24545d4b69
Goldenegg000 <janiklehmann30@gmail.com>
2023-06-01 12:32:37 +0200

    basics

---
+++ script.js
@@ -0,0 +1,4 @@
+var HomeButton = document.getElementById("HomeButton");
+var VideosButton = document.getElementById("VideosButton");
+VideosButton.addEventListener("click", () => window.location.replace("Watch.html"));
+HomeButton.addEventListener("click", () => window.location.replace("Home.html"));
